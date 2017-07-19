ed72f239094ec1684894cbc7cdda02cbb305f24a
JiZhenRu <dsdgho@live.com>
2017-07-19 17:37:18 +0800

    动态建表

--- src/main/java/com/vvhcc/test/ConfigTest.java
+++ src/main/java/com/vvhcc/test/ConfigTest.java
@@ -3,35 +3,30 @@ package com.vvhcc.test;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Properties;
 
+import com.vvhcc.utils.ConnectionUtils;
+import com.vvhcc.utils.CreateTableUtils;
+
 public class ConfigTest {
 	public static void main(String[] args) throws Exception {
-		Properties prop1 = new Properties();
 		Properties prop2 = new Properties();
 		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-		InputStream configin = classloader.getResourceAsStream("config.properties");
 		InputStream sqlin = classloader.getResourceAsStream("sql.properties");
-		//InputStream in = new BufferedInputStream(new FileInputStream("config.properties"));
-		prop1.load(configin);
 		prop2.load(sqlin);
 		/*Iterator<String> iterator = properties.stringPropertyNames().iterator();
 		while(iterator.hasNext()){
 			String key = iterator.next();
 			System.out.println(key+":"+properties.getProperty(key));
 		}*/
-		String url = prop1.getProperty("url");
-		String driver = prop1.getProperty("driver");
-		String username = prop1.getProperty("username");
-		String password = prop1.getProperty("password");
-		Class.forName(driver);
-		Connection connection = DriverManager.getConnection(url, username, password);
-		Statement statement = connection.createStatement();
+		Connection connection = ConnectionUtils.getConnection("jdbc.properties");
 		
 		String sql = prop2.getProperty("sql");
-		ResultSet resultSet = statement.executeQuery(sql);
+		PreparedStatement ps = connection.prepareStatement(sql);
+		ResultSet resultSet = ps.executeQuery(sql);
 		
 		while(resultSet.next()){
 			int id = resultSet.getInt("id");
@@ -39,7 +34,13 @@ public class ConfigTest {
 			System.out.println(id+name);
 		}
 		
-//		System.out.println(url);
-//		System.out.println(sql);
+		Boolean b = CreateTableUtils.createTable();
+		System.out.println(b);
+		
+		//关闭流
+		ConnectionUtils.closeResultSet(resultSet);
+		ConnectionUtils.closePreparedStatement(ps);
+		ConnectionUtils.closeConnection(connection);
+
 	}
 }
