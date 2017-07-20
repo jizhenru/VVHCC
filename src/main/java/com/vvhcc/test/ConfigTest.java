0e705a251df0e10501fef896215d90f7303add82
JiZhenRu <dsdgho@live.com>
2017-07-20 17:36:26 +0800

    动态生成sql语句

--- src/main/java/com/vvhcc/test/ConfigTest.java
+++ src/main/java/com/vvhcc/test/ConfigTest.java
@@ -2,20 +2,24 @@ package com.vvhcc.test;
 
 import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import com.vvhcc.utils.ConnectionUtils;
-import com.vvhcc.utils.CreateTableUtils;
 
 public class ConfigTest {
 	public static void main(String[] args) throws Exception {
+		Properties prop1 = new Properties();
 		Properties prop2 = new Properties();
 		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
 		InputStream sqlin = classloader.getResourceAsStream("sql.properties");
+		InputStream table = classloader.getResourceAsStream("Table.properties");
+		prop1.load(table);
 		prop2.load(sqlin);
 		/*Iterator<String> iterator = properties.stringPropertyNames().iterator();
 		while(iterator.hasNext()){
@@ -28,14 +32,34 @@ public class ConfigTest {
 		PreparedStatement ps = connection.prepareStatement(sql);
 		ResultSet resultSet = ps.executeQuery(sql);
 		
+		//获取字段集合
+		List<String> fieldlist = new ArrayList<String>();
+		String id = prop1.getProperty("id");
+		fieldlist.add(id);
+		String tablename = prop1.getProperty("tablename");
+		String field = prop1.getProperty("field");
+		String[] fields = field.split(",");
+		for (String string : fields) {
+			fieldlist.add(string);
+		}
+		List<Map> list = new ArrayList<Map>();
 		while(resultSet.next()){
-			int id = resultSet.getInt("id");
-			String name = resultSet.getString("name");
-			System.out.println(id+name);
+//			int id = resultSet.getInt("id");
+//			String id = resultSet.getString("id");
+//			String name = resultSet.getString("name");
+//			System.out.println(id+name);
+			Map<String,String> map = new HashMap<String, String>();
+			for (String string : fieldlist) {
+				String str = resultSet.getString(string);
+				map.put(string, resultSet.getString(string));
+//				System.out.println(str);
+			}
+			list.add(map);
 		}
 		
-		Boolean b = CreateTableUtils.createTable();
-		System.out.println(b);
+		System.out.println(list);
+//		Boolean b = CreateTableUtils.createTable();
+//		System.out.println(b);
 		
 		//关闭流
 		ConnectionUtils.closeResultSet(resultSet);
--- src/main/java/com/vvhcc/utils/CreateSqlUtils.java
+++ src/main/java/com/vvhcc/utils/CreateSqlUtils.java
@@ -1,9 +1,32 @@
 package com.vvhcc.utils;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class CreateSqlUtils {
 	
 	public static Boolean createSql(){
-		
+		Properties prop = new Properties();
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		InputStream in = classLoader.getResourceAsStream("Table.properties");
+		String tablename = prop.getProperty("tablename");
+		String id = prop.getProperty("id");
+		String fieldString = prop.getProperty("field");
+//		String[] fields = fieldString.split(",");
+		String querySql ="select "+id+","+ fieldString +" form "+tablename;
 		return null;
+	}
+	public static void main(String[] args) throws IOException {
+		Properties prop = new Properties();
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		InputStream in = classLoader.getResourceAsStream("Table.properties");
+		prop.load(in);
+		String tablename = prop.getProperty("tablename");
+		String id = prop.getProperty("id");
+		String fieldString = prop.getProperty("field");
+//		String[] fields = fieldString.split(",");
+		String querySql ="select "+id+","+ fieldString +" form "+tablename;
+		System.out.println(querySql);
 	}
 }
